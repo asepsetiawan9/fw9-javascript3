4792575ea69e40c8547ead78c40ef2f8674ffe09
asetiawan9 <zep.setiawan09@gmail.com>
2022-06-23 15:24:03 +0700

    fix and rename file of programs

--- fetch.js
+++ fetch.js
@@ -1,3 +1,28 @@
-fetch('http://example.com/movies.json')
-  .then(response => response.json())
-  .then(data => console.log(data));
+async function getNameData(){
+
+    const request = await fetch('https://jsonplaceholder.typicode.com/users')
+    //const request = await fetch(' Https://rickandmortyapi.com/api/character/1')
+    const data = await request.json()
+    try{ 
+      const results = await data() 
+      // setTimeout(()=>{
+      //     const showMonth = sendCB.map(x => x.toLowerCase());
+      //     console.log(showMonth);
+      // }, 5000)
+    }
+  // }catch(err){ 
+  //     switch(err.message){ 
+  //         case 'Input Bulan': { 
+  //             console.log('failed, ', err.message)  
+  //             break
+  //         }
+  //     }
+  // }
+    // .then(data => {
+    //     data.map(elment => {
+    //         console.log(elment.name)
+    //     })
+    // }).catch()
+}
+
+getNameData()
